753b49013e3c64ecba78f92bfefee0f177b5e03b
Anathalia Izidro <49588396+AnathaliaIzidro@users.noreply.github.com>
2024-07-18 14:23:28 -0300

    Create alunos.py

---
+++ alunos.py
@@ -0,0 +1,9 @@
+escola = input("Digite o nome da escola: ") #String
+
+quantidadeDeAlunos = int(input('Digite a quantidade de alunos: '))
+
+media = float(input('Digite a média da turma: '))
+
+print(f'Nome da escola: {escola}')
+print(f'Quantidade de alunos: {quantidadeDeAlunos}')
+print(f'Média: {media}')
